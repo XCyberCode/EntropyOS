930e37ff877171005c100f655928aaef48648c61
XCyberCode <mat.vasenin@yandex.ru>
2023-08-08 20:59:22 +0300

    Update app framework

--- src/core/AppFramework.hpp
+++ src/core/AppFramework.hpp
@@ -19,10 +19,9 @@ class Button
     String textAnchorV = "center"; // Widget vertical text position. Can be: up, center, down
     String textAnchorH = "center"; // Widget horizontal text position. Can be: right, center, left
     bool isHighlighted = false; // If true, a button's content color will be inverted
-    // REPLACED. String frameType = "rectangle"; // Button border style. Can be: none, rectangle, rounded, round
     bool isFramed = true; // If true, a border will be drawn
     short cornerRadius = 0; // Corner radius
-    // bool isFramedOutside = false; // If true, a border will be drawn around widget
+    // UNUSED. bool isFramedOutside = false; // If true, a border will be drawn around widget
 
     // Methods
     void draw(); // Draw configured widget
@@ -36,8 +35,10 @@ class TextLabel
     short width, height; // Widget size
     short positionX, positionY; // Widget position
     String textAnchor = "center"; // Widget text position. Can be: left, center, right
-    String frameType = "none"; // Widget border type. Can be: none, rectangle, rounded, round
-    bool isFramedOutside = false; // If true, a border will be drawn around the widget
+    bool isFramed = true; // If true, a border will be drawn
+    short cornerRadius = 0; // Corner radius
+    // DEPRECATED. String frameType = "none"; // Widget border type. Can be: none, rectangle, rounded, round
+    // UNUSED. bool isFramedOutside = false; // If true, a border will be drawn around the widget
     
     // Methods
     void draw(); // Draw configured widget
@@ -49,8 +50,7 @@ class Icon
     // Vars
     short width, height; // Widget size
     short positionX, positionY; // Widget position
-    String frameType = "none"; // Widget border type. Can be: none, rectangle, rounded, round
-    bool isFramedOutside = false; // If true, a border will be drawn around the widget
+    bool isFramed = true; // If true, a border will be drawn
 
     // Methods
     void draw(unsigned char image[]); // Draw configured widget. The target icon is excepted as an argument
