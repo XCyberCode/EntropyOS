1934dacbbc1220d8bfe1fe0fc81befd10de1858d
XCyberCode <mat.vasenin@yandex.ru>
2023-08-11 20:00:39 +0000

    Add debug app

---
+++ src/apps/Debug.cpp
@@ -0,0 +1,13 @@
+// Header
+#include <apps/Debug.hpp>
+
+// Local dependencies
+#include <core/AppFramework.hpp>
+#include <core/HardwareManager.hpp>
+
+Button upBtnWidget;
+
+void Debug::draw()
+{
+
+}
